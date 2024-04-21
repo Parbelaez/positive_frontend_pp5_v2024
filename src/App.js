5e9f47864b9435d8faaddb291ae09103b5f9d987
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2024-04-21 09:36:10 +0200

    Create the NavBar routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { Container } from 'react-bootstrap';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,27 +5,34 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo_color_trimmed from "../assets/logo_color_trimmed.png";
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () =>{
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container fluid className="ms-5 me-5">
-                <Navbar.Brand href="#">
-                    <img
-                        src={logo_color_trimmed}
-                        alt="Positive Logo"
-                        height="45"
-                    />
-                </Navbar.Brand>
+                <NavLink exact to="/">
+                    <Navbar.Brand>
+                        <img
+                            src={logo_color_trimmed}
+                            alt="Positive Logo"
+                            height="45"
+                        />
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
-                        className="me-auto my-2 my-lg-0"
+                        className="me-auto my-2 my-lg-0 text-center"
                         style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Sign In</Nav.Link>
-                        <Nav.Link href="#action2">About</Nav.Link>
+                        <Nav.Link as={ NavLink } to="/login">
+                            Login
+                        </Nav.Link>
+                        <Nav.Link as={ NavLink } to="/about">
+                            About
+                        </Nav.Link>
                     </Nav>
                     <Form className="d-flex">
                         <Form.Control
