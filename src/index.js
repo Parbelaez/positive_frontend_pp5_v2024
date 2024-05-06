52498b11655c40ec8e9b4b8910c39ea374302ab2
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2024-05-06 05:13:23 +0200

    Add the strict mode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import { CurrentUserProvider } from './contexts/CurrentUserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <CurrentUserProvider>
-      <App />
-    </CurrentUserProvider>
-  </Router>
+  <React.StrictMode>
+    <Router>
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
+      </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
