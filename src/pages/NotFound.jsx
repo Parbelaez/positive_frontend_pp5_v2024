713e8aaac74b5c688a4286620ed20271414be9f6
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2024-05-06 02:19:02 +0200

    Add the home link the page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,20 @@
 import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function NotFound() {
-  return (
-    <Container>
-        <Row className="justify-content-md-center">
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
                 <Col md="auto">
                     <h1>PAGE NOT FOUND</h1>
                     <p>
-                        Sorry, the page you are looking for does not exist.
-                        But, not to worry. You can always go back to the home page.
+                        Sorry, the page you are looking for does not exist. But,
+                        not to worry. You can always go back to the{" "}
+                        <Link to={"/"}>home page</Link>.
                     </p>
                 </Col>
-        </Row>
-    </Container>
+            </Row>
+        </Container>
     );
 }
 
